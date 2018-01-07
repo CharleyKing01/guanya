a0a15e27ff21544ddcef474279805cbf64f74823
lql <www.linqiaoli.com@qq.com>
2018-01-07 16:45:05 +0800

    update publish

--- src/main/java/org/charley/CharleyApplication.java
+++ src/main/java/org/charley/CharleyApplication.java
@@ -2,6 +2,8 @@ package org.charley;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("org.charley.mapper")
@@ -11,3 +13,16 @@ public class CharleyApplication {
 		SpringApplication.run(CharleyApplication.class, args);
 	}
 }
+
+//打包使用
+//public class CharleyApplication extends SpringBootServletInitializer {
+//
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(CharleyApplication.class);
+//	}
+//
+//	public static void main(String[] args) throws Exception {
+//		SpringApplication.run(CharleyApplication.class, args);
+//	}
+//}
